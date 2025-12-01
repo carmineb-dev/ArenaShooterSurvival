5aa5a56dd3be770dcfb33bb1dd180dfe18cd93dd
carmineb-dev <245718570+carmineb-dev@users.noreply.github.com>
2025-12-01 18:17:56 +0100

    Day 2 - Player rotation

--- ArenaShooterSurvival/Assets/Scripts/Player/PlayerController.cs
+++ ArenaShooterSurvival/Assets/Scripts/Player/PlayerController.cs
@@ -14,10 +14,28 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    private void LateUpdate()
     {
-        //Movement
+        // Player movement
         playerRb.linearVelocity = moveInput * speed;
+
+        RotatePlayer();
+    }
+
+    // Rotate Player
+    public void RotatePlayer()
+    {
+        // Calculate Mouse Position
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Calculate distance between player and mouse
+        Vector2 direction = mousePosition - (Vector2)transform.position;
+
+        // Calculate angle between mouse and player (90 degree offset cause of the sprite)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+
+        // Rotate
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     public void Move(InputAction.CallbackContext context)
